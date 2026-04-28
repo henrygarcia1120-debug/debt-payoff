fdd8cfc57a5dadaef89ed6625fcc30f09545ccea
HENRY <henry.garcia1120@icloud.com>
2026-04-27 22:29:24 -0700

    implemented creditcard value class

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,23 @@
 public class CreditCard {
-    
+    private String name; 
+    private double apr; 
+    private double balance; 
+
+    @Override
+    public String toString() 
+    {
+        return "CreditCard [name= " + name + ", apr= " + apr + ", balance= " + balance + "]";
+    }
+
+    public CreditCard(String name, double apr, double balance)
+    {
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance; 
+    }
+    public String getName()
+    {
+        return name;
+    }
+
 }
--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -8,7 +8,13 @@ public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        CreditCard amex = new CreditCard("American Express", 18.4, 402);
+        System.out.println(amex);
+
+
         List<Double> aprs = new ArrayList<>(); 
+        List<CreditCard> cards = new ArrayList<>();
+
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -17,6 +23,8 @@ public class PayoffApp {
             aprs.add(apr);
 
             double balance = scan.nextDouble();
+            
+            cards.add(new CreditCard(name, apr, balance);
 
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
@@ -25,8 +33,11 @@ public class PayoffApp {
             String balanceString = String.format("$%.2f", balance);
             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
         }
+
         Collections.sort(aprs, Collections.reverseOrder());
-        
+        //Collections.sort(cards);
+
         System.out.println(aprs); 
+        System.out.println(cards); 
     }
 }
